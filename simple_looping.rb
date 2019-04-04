d97c9bcfbbc47ef1baff2a856050d03bed62d655
Caleb Einarr <earthctzn1@gmail.com>
2019-04-04 21:00:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,13 +38,12 @@ until counter == number_of_times
 end
 
 def for_iterator(number_of_times)
-  counter = 0
   number_of_times = [1..10]
   for counter in number_of_times do
   phrase = "Welcome to Flatiron School's Web Development Course!"
   until counter == number_of_times
   puts phrase
-  #counter += 1
+    break
   end
 end
 end
